adf906d4c3b48dfb741fd30a6b71072f0db3db21
dekito12 <deaqow@gmail.com>
2023-03-08 17:57:56 -0800

    done with code it runs and pings

--- Server.py
+++ Server.py
@@ -1,10 +1,12 @@
+import os
+
 class Server:
     """ Server class for representing and manipulating servers. """
-
     def __init__(self, server_ip):
         # TODO -
         self.server_ip = server_ip
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        response = os.system(f"ping {self.server_ip} -n 5")
+        return response
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
+# Dekow
 # CNE 335 Fall
+import os
+
+class Server:
+    """ Server class for representing and manipulating servers. """
+
+    def __init__(self, server_ip):
+        self.server_ip = server_ip
+
+    def ping(self):
+        response = os.system("ping -c 5 " + self.server_ip)
+        return response == 0
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Dekow")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+    server = Server("35.161.200.181")  # create a server object
+    if server.ping():
+        print(server.ping())
+    else:
+        print("Server is not responding.")
