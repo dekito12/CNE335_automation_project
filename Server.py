a2603f8c5fafdac9872d8ddef086eb8e15a86a76
dekito12 <deaqow@gmail.com>
2023-03-21 19:33:32 +0300

    done with code it runs and pings

--- Server.py
+++ Server.py
@@ -3,40 +3,39 @@ import paramiko
 
 class Server:
     """ Server class for representing and manipulating servers. """
+
     def __init__(self, server_ip, key_file, username, upgrade_command):
+        # TODO -
         self.server_ip = server_ip
         self.username = username
         self.command = upgrade_command
         self.key_file = key_file
 
+
     def ping(self):
-        result = os.system("ping -n 5 %s" % self.server_ip)
+        # TODO - Use os module to ping the server
+        result = os.system("ping -n 5 %s" % self.server_ip) #https://stackoverflow.com/questions/48468135/baccess-denied-option-c-requires-administrative-privileges
         return result
 
-    def ssh_command(self):
+    def upgrade(self):
+        # TODO - Use os module to ping the server
+        #https://www.linode.com/docs/guides/use-paramiko-python-to-ssh-into-a-server/
+        # Create the ssh client
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #https://stackoverflow.com/questions/10825687/python-paramiko-getting-error-object-has-no-attribute-get-fingerprint
         k = paramiko.RSAKey.from_private_key_file(self.key_file)
+        # Make the connection to the server
         ssh_client.connect(hostname=self.server_ip, username=self.username, pkey=k)
 
+        # execute the command
+        #https://blog.ruanbekker.com/blog/2018/04/23/using-paramiko-module-in-python-to-execute-remote-bash-commands/#:~:text=Using%20Paramiko%20Module%20in%20Python%20to%20Execute%20Remote,lsb_release%20-a%20on%20our%20remote%20server%2C%20side-b%3A%20
         stdin, stdout, stderr = ssh_client.exec_command(self.command)
 
-        for line in stdout.read().splitlines():
-            print(line)
+        # Return the result from both stdout and error
+        result = stdout.readlines() + stderr.readlines()
 
+        # Disconnect
         ssh_client.close()
 
-# create a Server instance with the required arguments
-server = Server('35.161.200.181', '/Users/dekowali/.ssh/id_rsa', 'ubuntu', 'ls -l /tmp')
-
-# ping the server to check connectivity
-ping_result = server.ping()
-if ping_result == 0:
-    print('Server is reachable.')
-else:
-    print('Server is not reachable.')
-
-# execute the command on the server using SSH
-print('Executing command on the server...')
-server.ssh_command()
-print('Command execution completed.')
+        return result
--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import os
-import paramiko
-
-class Server:
-    """ Server class for representing and manipulating servers. """
-
-    def __init__(self, server_ip, rsa_key_file):
-        self.server_ip = server_ip
-        self.rsa_key_file = rsa_key_file
-        self.ssh_client = paramiko.SSHClient()
-        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh_client.connect(hostname=self.server_ip, username='ubuntu', key_filename=self.rsa_key_file)
-
-    def ping(self):
-        response = os.system("ping -c 5 " + self.server_ip)
-        return response == 0
+from Server import Server
 
 
 def print_program_info():
@@ -24,9 +9,10 @@ if __name__ == '__main__':
     print_program_info()
     my_server_ip = "35.161.200.181"
     my_rsa_key_file = "/Users/dekowali/.ssh/id_rsa"
-    server = Server(my_server_ip, my_rsa_key_file)
-    if server.ping():
-        print("Server is reachable.")
-    else:
-        print("Server is not reachable.")
-    server.disconnect()
+    username = "ubuntu"
+    my_upgrade_command = 'sudo apt update && sudo apt upgrade -y'
+    my_server = Server(my_server_ip, my_rsa_key_file, username, my_upgrade_command)
+    print(my_server.ping())
+    print("Updating server")
+    ssh_result = my_server.upgrade()
+    print(''.join(ssh_result))
