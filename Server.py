420928ae76e35959c13f93df5da6a0deda704da3
dekito12 <deaqow@gmail.com>
2023-03-21 14:40:00 +0300

    done with code it runs and pings

--- Server.py
+++ Server.py
@@ -1,12 +1,42 @@
 import os
+import paramiko
 
 class Server:
     """ Server class for representing and manipulating servers. """
-    def __init__(self, server_ip):
-        # TODO -
+    def __init__(self, server_ip, key_file, username, upgrade_command):
         self.server_ip = server_ip
+        self.username = username
+        self.command = upgrade_command
+        self.key_file = key_file
 
     def ping(self):
-        # TODO - Use os module to ping the server
-        response = os.system(f"ping {self.server_ip} -n 5")
-        return response
+        result = os.system("ping -n 5 %s" % self.server_ip)
+        return result
+
+    def ssh_command(self):
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        k = paramiko.RSAKey.from_private_key_file(self.key_file)
+        ssh_client.connect(hostname=self.server_ip, username=self.username, pkey=k)
+
+        stdin, stdout, stderr = ssh_client.exec_command(self.command)
+
+        for line in stdout.read().splitlines():
+            print(line)
+
+        ssh_client.close()
+
+# create a Server instance with the required arguments
+server = Server('35.161.200.181', '/Users/dekowali/.ssh/id_rsa', 'ubuntu', 'ls -l /tmp')
+
+# ping the server to check connectivity
+ping_result = server.ping()
+if ping_result == 0:
+    print('Server is reachable.')
+else:
+    print('Server is not reachable.')
+
+# execute the command on the server using SSH
+print('Executing command on the server...')
+server.ssh_command()
+print('Command execution completed.')
--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-# This is the template code for the CNE335 Final Project
-# Dekow
-# CNE 335 Fall
 import os
+import paramiko
 
 class Server:
     """ Server class for representing and manipulating servers. """
 
-    def __init__(self, server_ip):
+    def __init__(self, server_ip, rsa_key_file):
         self.server_ip = server_ip
+        self.rsa_key_file = rsa_key_file
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh_client.connect(hostname=self.server_ip, username='ubuntu', key_filename=self.rsa_key_file)
 
     def ping(self):
         response = os.system("ping -c 5 " + self.server_ip)
         return response == 0
 
+
 def print_program_info():
-    # TODO - Change your name
     print("Server Automator v0.1 by Dekow")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    server = Server("35.161.200.181")  # create a server object
+    my_server_ip = "35.161.200.181"
+    my_rsa_key_file = "/Users/dekowali/.ssh/id_rsa"
+    server = Server(my_server_ip, my_rsa_key_file)
     if server.ping():
-        print(server.ping())
+        print("Server is reachable.")
     else:
-        print("Server is not responding.")
+        print("Server is not reachable.")
+    server.disconnect()
